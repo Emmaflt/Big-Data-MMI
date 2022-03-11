576433efcf562d3f3ec9eefc9eb5399c257309d1
emmaflt <emmafouillat@gmail.com>
2022-03-11 10:03:57 +0100

    ajout btn download pour tout le monde

--- js/download_prints.js
+++ js/download_prints.js
@@ -8,8 +8,8 @@ document.getElementById("button4").addEventListener('click', function(){ // Ajou
     a.click(); //dl du fichier auto
 });
 
-document.getElementById("button4").addEventListener('click', function(){ // Ajout d'un event lors du clic du btn
-    var url_base64jp = document.getElementById("chart4").toDataURL("image/jpg"); // transform du graph en image
+document.getElementById("button3").addEventListener('click', function(){ // Ajout d'un event lors du clic du btn
+    var url_base64jp = document.getElementById("chart3").toDataURL("image/jpg"); // transform du graph en image
     var a = document.getElementById("download"); //Creation du <a>
     // console.log(a);
     a.href = url_base64jp; // on stock l'url base64 dans le lien
@@ -17,8 +17,8 @@ document.getElementById("button4").addEventListener('click', function(){ // Ajou
     a.click(); //dl du fichier auto
 });
 
-document.getElementById("button4").addEventListener('click', function(){ // Ajout d'un event lors du clic du btn
-    var url_base64jp = document.getElementById("chart4").toDataURL("image/jpg"); // transform du graph en image
+document.getElementById("button1").addEventListener('click', function(){ // Ajout d'un event lors du clic du btn
+    var url_base64jp = document.getElementById("chartPie").toDataURL("image/jpg"); // transform du graph en image
     var a = document.getElementById("download"); //Creation du <a>
     // console.log(a);
     a.href = url_base64jp; // on stock l'url base64 dans le lien
@@ -26,8 +26,8 @@ document.getElementById("button4").addEventListener('click', function(){ // Ajou
     a.click(); //dl du fichier auto
 });
 
-document.getElementById("button4").addEventListener('click', function(){ // Ajout d'un event lors du clic du btn
-    var url_base64jp = document.getElementById("chart4").toDataURL("image/jpg"); // transform du graph en image
+document.getElementById("button2").addEventListener('click', function(){ // Ajout d'un event lors du clic du btn
+    var url_base64jp = document.getElementById("chart2").toDataURL("image/jpg"); // transform du graph en image
     var a = document.getElementById("download"); //Creation du <a>
     // console.log(a);
     a.href = url_base64jp; // on stock l'url base64 dans le lien
@@ -35,8 +35,8 @@ document.getElementById("button4").addEventListener('click', function(){ // Ajou
     a.click(); //dl du fichier auto
 });
 
-document.getElementById("button4").addEventListener('click', function(){ // Ajout d'un event lors du clic du btn
-    var url_base64jp = document.getElementById("chart4").toDataURL("image/jpg"); // transform du graph en image
+document.getElementById("button5").addEventListener('click', function(){ // Ajout d'un event lors du clic du btn
+    var url_base64jp = document.getElementById("chart5").toDataURL("image/jpg"); // transform du graph en image
     var a = document.getElementById("download"); //Creation du <a>
     // console.log(a);
     a.href = url_base64jp; // on stock l'url base64 dans le lien
